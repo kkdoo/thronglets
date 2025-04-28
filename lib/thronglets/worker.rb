b0f9022cf49d70502c24d43542af68781f763b21
Artem Maschenko <artem.maschenko@gmail.com>
2025-04-29 01:09:56 +0300

    Pool sizes for worker

--- lib/thronglets/worker.rb
+++ lib/thronglets/worker.rb
@@ -32,18 +32,10 @@ module Thronglets
 
       def worker
         @worker ||= Temporal::Worker.new(
-          ## how many threads poll for activities
-          #activity_thread_pool_size: 20,
-          ## how many threads poll for workflows
-          #workflow_thread_pool_size: 10,
-          ## identifies the version of workflow worker code
-          #binary_checksum: nil,
-          ## how many seconds to wait after unsuccessful poll for activities
-          #activity_poll_retry_seconds: 0,
-          ## how many seconds to wait after unsuccessful poll for workflows
-          #workflow_poll_retry_seconds: 0,
-          ## rate-limit for starting activity tasks (new activities + retries) on the task queue
-          #activity_max_tasks_per_second: 0,
+          # how many threads poll for activities
+          activity_thread_pool_size: 20,
+          # how many threads poll for workflows
+          workflow_thread_pool_size: 10,
         )
       end
 
